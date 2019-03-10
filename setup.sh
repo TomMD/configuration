30652e7f3f7d83c2a432dfb925652bb8c998eb6b
Thomas M. DuBuisson <tommd@musedev.io>
2019-03-09 21:19:16 -0800

    headless

--- setup.sh
+++ setup.sh
@@ -14,7 +14,7 @@ if [[ -x "$(which nvim)" ]] ; then
     fi
     if [[ -x "$(which pip3)" ]] ; then
         pip3 install neovim
-        nvim +NeoBundleInstall +qall
+        nvim +NeoBundleInstall +qall --headless
     fi
 fi
 
