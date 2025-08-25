3597c7cdaab6060093ef9b3011f344807cd4b6c3
Thomas M. DuBuisson <tdubuisson@paxos.com>
2025-08-24 21:11:31 -0700

    tmux binding change and slurp raw init.lua file

--- flake.nix
+++ flake.nix
@@ -107,6 +107,9 @@
                 bindkey -M viins 'jk' vi-cmd-mode
                 bindkey '^R' history-incremental-search-backward
                 
+                # Accept autosuggestion with Ctrl+Space
+                bindkey '^ ' autosuggest-accept
+                
                 # Source private config if it exists
                 if [ -e "$HOME/.zshrc_private_stuff" ]; then
                   source "$HOME/.zshrc_private_stuff"
@@ -119,6 +122,7 @@
               shellAliases = {
                 ls = if pkgs.stdenv.isDarwin then "ls -G" else "ls --color -G";
                 o = if pkgs.stdenv.isDarwin then "open" else "xdg-open";
+                vi = "nvim";
               };
             };
 
@@ -170,6 +174,23 @@
               defaultEditor = true;
               viAlias = true;
               vimAlias = true;
+              withNodeJs = true;
+              withPython3 = true;
+              
+              # Read the local init.lua file and use it as extraLuaConfig
+              extraLuaConfig = builtins.readFile ./init.lua;
+              
+              extraPackages = with pkgs; [
+                # LSP servers and tools (already included above but explicit here)
+                haskell-language-server
+                kotlin-language-server
+                gopls
+                rust-analyzer
+                lua-language-server
+                nil
+                typescript-language-server
+                nixpkgs-fmt
+              ];
               
               plugins = with pkgs.vimPlugins; [
                 # Core dependencies
@@ -178,7 +199,7 @@
                 async-vim
                 
                 # LSP and language support
-                nvim-lspconfig
+		nvim-lspconfig
                 rust-tools-nvim
                 nvim-jdtls
                 
@@ -242,204 +263,6 @@
                 vim-vsnip
               ];
 
-              extraLuaConfig = ''
-                -- General settings
-                vim.g.mapleader = " "
-                vim.g.maplocalleader = "\\"
-                vim.lsp.set_log_level("info")
-
-                -- Basic options
-                vim.opt.backspace = 'indent,eol,start'
-                vim.opt.incsearch = true
-                vim.opt.hlsearch = true
-                vim.opt.ignorecase = true
-                vim.opt.smartcase = true
-                vim.opt.ruler = true
-                vim.opt_local.spelllang = 'en_us'
-                vim.opt.smarttab = true
-                vim.opt.shiftround = true
-                vim.opt.switchbuf = 'useopen'
-
-                vim.g.airline_powerline_fonts = true
-                vim.g.ctrlp_user_command = 'git -C %s ls-files'
-
-                -- Keymaps
-                -- Tab navigation
-                vim.keymap.set('n', '<C-n>', ':tabnext<Enter>')
-                vim.keymap.set('n', '<C-b>', 'gT')
-
-                -- Clear search highlighting
-                vim.keymap.set('n', '<Leader><Leader><Leader>', ':noh<Enter>')
-
-                -- Movement and editing
-                vim.keymap.set('i', 'jk', '<Esc>')
-                vim.keymap.set('i', '<C-J>', '<C-W><C-J>')
-                vim.keymap.set('i', '<C-K>', '<C-W><C-K>')
-                vim.keymap.set('i', '<C-L>', '<C-W><C-L>')
-                vim.keymap.set('i', '<C-H>', '<C-W><C-H>')
-                vim.keymap.set("t", "jk", "<C-\\><C-n>", { noremap = true, silent = true, desc = "Exit Terminal Mode" })
-
-                -- Window navigation
-                vim.keymap.set('n', '<C-k>', '<C-w>k')
-                vim.keymap.set('n', '<C-j>', '<C-w>j')
-                vim.keymap.set('n', '<C-h>', '<C-w>h')
-                vim.keymap.set('n', '<C-l>', '<C-w>l')
-
-                -- Tag navigation
-                vim.keymap.set('n', '<silent><C-[>', '<C-w><C-]><C-w>T')
-
-                -- Copilot
-                vim.keymap.set('v', '<space>c', 'copilot#Accept("\\<CR>")', { expr = true })
-
-                -- LSP diagnostics keymaps
-                local opts = { noremap=true, silent=true }
-                vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
-                vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
-                vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
-                vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
-
-                -- LSP on_attach function
-                local on_attach = function(client, bufnr)
-                  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
-
-                  local bufopts = { noremap=true, silent=true, buffer=bufnr }
-                  vim.keymap.set('n', '<space>ws', '<cmd>lua vim.lsp.buf.workspace_symbol()<CR>', { desc = 'Workspace Symbols' })
-                  vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
-                  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
-                  vim.keymap.set('n', 'gc', vim.lsp.buf.incoming_calls)
-                  vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
-                  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
-                  vim.keymap.set('n', '<C-s>', vim.lsp.buf.signature_help, bufopts)
-                  vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
-                  vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
-                  vim.keymap.set('n', '<space>wl', function()
-                    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-                  end, bufopts)
-                  vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
-                  vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
-                  vim.keymap.set('n', '<space>a', vim.lsp.buf.code_action, bufopts)
-                  vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
-                end
-
-                -- LSP configurations
-                local lspconfig = require('lspconfig')
-
-                -- Haskell
-                require('lspconfig').hls.setup{
-                    on_attach = on_attach,
-                    flags = lsp_flags,
-                    settings = {
-                       haskell = {
-                        formattingProvider = "fourmolu"
-                      }
-                  }
-                }
-
-                -- Kotlin
-                require('lspconfig').kotlin_language_server.setup{
-                    on_attach = on_attach
-                }
-
-                -- Go
-                require('lspconfig').gopls.setup{
-                    cmd = {"gopls", "serve"},
-                    filetypes = {"go", "gomod"},
-                    root_dir = require('lspconfig/util').root_pattern("go.work", "go.mod", ".git"),
-                    settings = {
-                      gopls = {
-                        analyses = {
-                          unusedparams = true,
-                        },
-                        staticcheck = true,
-                      },
-                    },
-                    on_attach = on_attach
-                }
-
-                -- Rust
-                local rt = require("rust-tools").setup({
-                    tools = {
-                        inlay_hints = {
-                            only_current_line = true,
-                        }
-                    },
-                    server = {
-                        on_attach = on_attach,
-                    }
-                })
-
-                -- Nix
-                lspconfig.nil_ls.setup{
-                    on_attach = on_attach,
-                    settings = {
-                      ['nil'] = {
-                        formatting = {
-                          command = { "nixpkgs-fmt" },
-                        },
-                      },
-                    },
-                }
-
-                -- Lua (for Neovim config)
-                lspconfig.lua_ls.setup{
-                    on_attach = on_attach,
-                    settings = {
-                      Lua = {
-                        runtime = {
-                          version = 'LuaJIT',
-                        },
-                        diagnostics = {
-                          globals = {'vim'},
-                        },
-                        workspace = {
-                          library = vim.api.nvim_get_runtime_file("", true),
-                        },
-                        telemetry = {
-                          enable = false,
-                        },
-                      },
-                    },
-                }
-
-                -- TypeScript/JavaScript
-                lspconfig.ts_ls.setup{
-                    on_attach = on_attach,
-                }
-
-                -- Completion setup
-                vim.o.completeopt = "menuone,noinsert,noselect"
-                vim.opt.shortmess = vim.opt.shortmess + "c"
-
-                local cmp = require("cmp")
-                cmp.setup({
-                  preselect = cmp.PreselectMode.None,
-                  snippet = {
-                    expand = function(args)
-                      vim.fn["vsnip#anonymous"](args.body)
-                    end,
-                  },
-                  mapping = {
-                    ["<C-p>"] = cmp.mapping.select_prev_item(),
-                    ["<C-n>"] = cmp.mapping.select_next_item(),
-                    ["<S-Tab>"] = cmp.mapping.select_prev_item(),
-                    ["<Tab>"] = cmp.mapping.select_next_item(),
-                    ["<C-d>"] = cmp.mapping.scroll_docs(-4),
-                    ["<C-f>"] = cmp.mapping.scroll_docs(4),
-                    ["<C-Space>"] = cmp.mapping.complete(),
-                    ["<C-e>"] = cmp.mapping.close(),
-                    ["<CR>"] = cmp.mapping.confirm({
-                      behavior = cmp.ConfirmBehavior.Insert,
-                      select = true,
-                    }),
-                  },
-                  sources = {
-                    { name = "nvim_lsp" },
-                    { name = "vsnip" },
-                    { name = "path" },
-                    { name = "buffer" },
-                  },
-                })
-              '';
             };
 
             # Ripgrep configuration
